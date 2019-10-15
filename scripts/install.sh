9d766886cf81593de5df1e8c24d9a3f469f141ee
Kudryavtsev Kirill <kudryavtsev@nppsatek.ru>
2019-10-15 19:19:36 +0300

    fix(install.sh): remove installing system packages

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ git submodule init
 git submodule update
 
 echo "Installing deps..."
-apt-get install python3 python3-dev -y
-apt install git curl python3-pip python3-mysqldb python-dev libmysqlclient-dev -y
+# apt-get install python3 python3-dev -y
+# apt install git curl python3-pip python3-mysqldb python-dev libmysqlclient-dev -y
 
 pip3 install -r requirements.txt
