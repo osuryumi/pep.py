1ab63b39a5d912a40197001dfd8f5ba3a70b1c50
Kudryavtsev Kirill <kudryavtsev@nppsatek.ru>
2019-10-15 19:16:18 +0300

    feat: submodules

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+set -e
+
+echo "Fetching submosules"
+git submodule init
+git submodule update
+
 echo "Installing deps..."
 apt-get install python3 python3-dev -y
 apt install git curl python3-pip python3-mysqldb python-dev libmysqlclient-dev -y
---
+++ scripts/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+echo "Latest local commit"
+git log -1
+
+echo "Pulling changes from GitHub"
+git pull
+
+echo "Updating submodules"
+git submodule update
