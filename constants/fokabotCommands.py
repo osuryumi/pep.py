dd65d9d640553161c7a3219316bfcd65523dfe14
Kudryavtsev Kirill <kudryavtsev@nppsatek.ru>
2019-10-17 15:56:04 +0300

    Revert "Revert "fix(fokabot): ответ команды""
    This reverts commit 17fce7cfa662f2a55cf5895b29aaacbd2a03709e.

--- constants/fokabotCommands.py
+++ constants/fokabotCommands.py
@@ -1241,7 +1241,8 @@ def rank_map(fro, chan, message):
 		currentMap = token.tillerino[0]
 
 		glob.db.execute("update beatmaps set ranked = %s where beatmap_id = %s", [status_id, currentMap])
-		log.logMessage(f"Карте {currentMap}", discord="staff")
+		beatmapLink = f"({currentMap})[http://osu.ppy.sh/b/{currentMap}]"
+		log.logMessage(f"Статуст карты {beatmapLink} был изменен на {status} пользователем {token.username}", discord="staff")
 	except Exception as e:
 		return ":fire: ошибка при работе"
 	
