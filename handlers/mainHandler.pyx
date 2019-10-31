71a0b31a23d73c8813d3d19bf90c57b9e6b508d4
KotypeyPyEdition <39100756+KotypeyPyEdition@users.noreply.github.com>
2019-10-31 21:13:57 +0200

    Mini handler update :)

--- handlers/mainHandler.pyx
+++ handlers/mainHandler.pyx
@@ -243,24 +243,312 @@ class handler(requestsManager.asyncRequestHandler):
 	@tornado.web.asynchronous
 	@tornado.gen.engine
 	def asyncGet(self):
-		html = 	"<html><head><title>MA MAURO ESISTE?</title><style type='text/css'>body{width:30%}</style></head><body><pre>"
-		html += "           _                 __<br>"
-		html += "          (_)              /  /<br>"
-		html += "   ______ __ ____   ____  /  /____<br>"
-		html += "  /  ___/  /  _  \\/  _  \\/  /  _  \\<br>"
-		html += " /  /  /  /  /_) /  /_) /  /  ____/<br>"
-		html += "/__/  /__/  .___/  .___/__/ \\_____/<br>"
-		html += "        /  /   /  /<br>"
-		html += "       /__/   /__/<br>"
-		html += "<b>PYTHON > ALL VERSION</b><br><br>"
-		html += "<marquee style='white-space:pre;'><br>"
-		html += "                          .. o  .<br>"
-		html += "                         o.o o . o<br>"
-		html += "                        oo...<br>"
-		html += "                    __[]__<br>"
-		html += "    phwr-->  _\\:D/_/o_o_o_|__     <span style=\"font-family: 'Comic Sans MS'; font-size: 8pt;\">u wot m8</span><br>"
-		html += "             \\\"\"\"\"\"\"\"\"\"\"\"\"\"\"/<br>"
-		html += "              \\ . ..  .. . /<br>"
-		html += "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^<br>"
-		html += "</marquee><br><strike>reverse engineering a protocol impossible to reverse engineer since always</strike><br>we are actually reverse engineering bancho successfully. for the third time.<br><br><i>&copy; Ripple team, 2016</i></pre></body></html>"
+		html = """
+		<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>What are you doing here?</title>
+  <style type="text/css">
+  	html, body {
+  height: 100%;
+}
+
+body {
+  font-family: Helvetica, sans-serif;
+  overflow: hidden;
+  margin: 0;
+}
+
+.bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: url(https://i.ytimg.com/vi/SmQCpW72jDY/maxresdefault.jpg);
+  background-size: cover;
+  transform: scale(1.1);
+}
+
+.menu {
+  text-align: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  margin-top: -100px;
+  margin-left: 300px;
+}
+.menu .brand {
+  position: relative;
+  width: 200px;
+  height: 200px;
+  background-color: #ff66aa;
+  border-radius: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-size: 64px;
+  font-weight: bold;
+  animation: beat 0.3529411765s;
+  animation-iteration-count: infinite;
+  margin: 0 -50px;
+  z-index: 100;
+  border: 8px solid #fff;
+  box-shadow: 2px 0 4px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+}
+.menu .brand canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  mix-blend-mode: overlay;
+}
+.menu .brand span {
+  position: relative;
+  z-index: 1;
+}
+.menu .brand ~ .button:not(:first-of-type) {
+  margin-left: -30px;
+}
+.menu .button {
+  position: relative;
+  background: none;
+  border: none;
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  width: 200px;
+  height: 100px;
+  animation: un-hover 0.25s ease-in-out;
+  animation-fill-mode: forwards;
+}
+.menu .button:nth-of-type(1) {
+  z-index: 23;
+}
+.menu .button:nth-of-type(2) {
+  z-index: 22;
+}
+.menu .button:nth-of-type(3) {
+  z-index: 21;
+}
+.menu .button:nth-of-type(4) {
+  z-index: 20;
+}
+.menu .button:nth-of-type(5) {
+  z-index: 19;
+}
+.menu .button:nth-of-type(6) {
+  z-index: 18;
+}
+.menu .button:nth-of-type(7) {
+  z-index: 17;
+}
+.menu .button:nth-of-type(8) {
+  z-index: 16;
+}
+.menu .button:nth-of-type(9) {
+  z-index: 15;
+}
+.menu .button:nth-of-type(10) {
+  z-index: 14;
+}
+.menu .button:nth-of-type(11) {
+  z-index: 13;
+}
+.menu .button:nth-of-type(12) {
+  z-index: 12;
+}
+.menu .button:nth-of-type(13) {
+  z-index: 11;
+}
+.menu .button:nth-of-type(14) {
+  z-index: 10;
+}
+.menu .button:nth-of-type(15) {
+  z-index: 9;
+}
+.menu .button:nth-of-type(16) {
+  z-index: 8;
+}
+.menu .button:nth-of-type(17) {
+  z-index: 7;
+}
+.menu .button:nth-of-type(18) {
+  z-index: 6;
+}
+.menu .button:nth-of-type(19) {
+  z-index: 5;
+}
+.menu .button:nth-of-type(20) {
+  z-index: 4;
+}
+.menu .button span {
+  position: relative;
+  z-index: 1;
+  font-size: 14px;
+  color: #fff;
+  text-shadow: 0 4px 4px rgba(0, 0, 0, 0.2);
+}
+.menu .button:before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  transform: skew(-10deg);
+  box-shadow: 2px 0 4px rgba(0, 0, 0, 0.2);
+}
+.menu .button:hover {
+  animation: hover 0.25s ease-in-out;
+  animation-fill-mode: forwards;
+  margin-left: 0 !important;
+}
+.menu .button.button--green:before {
+  background-color: #6fa;
+}
+.menu .button.button--aqua:before {
+  background: #5af;
+}
+.menu .button.button--pink:before {
+  background: #f4427a;
+}
+.menu .button.button--purple:before {
+  background: #C400AB;
+}
+
+@keyframes beat {
+  from {
+    transform: scale(0.96);
+  }
+}
+@keyframes hover {
+  from {
+    width: 250px;
+  }
+  20% {
+    width: 220px;
+  }
+  40% {
+    width: 240px;
+  }
+  80% {
+    width: 235px;
+  }
+  100% {
+    width: 238px;
+  }
+}
+@keyframes un-hover {
+  from {
+    width: 180px;
+  }
+  20% {
+    width: 220px;
+  }
+  40% {
+    width: 190px;
+  }
+  80% {
+    width: 205px;
+  }
+  100% {
+    width: 200px;
+  }
+}
+  </style>
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="bg"></div>
+<div class="menu">
+   <div class="brand">
+      <canvas id="triangles" width="200" height="200"></canvas>
+      <span>osu!</span>
+   </div>
+  <a target="_blank" href="https://minase.tk" class="button button--pink">
+      <span>
+         Play
+      </span>
+   </a>
+  <a target="_blank" href="https://old.minase.tk" class="button button--aqua">
+      <span>
+         Old site
+      </span>
+   </a>
+   
+   <a target="_blank" href="https://vk.com/kotypey_vzloman" class="button button--purple">
+      <span>
+         VK
+      </span>
+   </a>
+   <a target="_blank" href="https://discord.gg/zPPDwcc" class="button button--green">
+      <span>
+         Discord
+      </span>
+   </a>
+</div>
+<!-- partial -->
+  <script>
+  	let c = document.querySelector('#triangles').getContext('2d')
+let canvas = c.canvas;
+let triangles = []
+
+let mx = 0, my = 0;
+let mouseHandler = event => {
+   let mx = event.pageX / 100
+   let my = event.pageY / 100
+   
+   document.querySelector('.bg').style.backgroundPosition = `${mx}px ${my}px`
+   document.querySelector('.menu').style.transform = `translate(${mx * 2}px, ${my * 2}px)`
+}
+
+let loop = () => {
+   c.clearRect(0, 0, canvas.width, canvas.height)
+   
+   triangles.forEach(triangle => {
+      let { x, y, brightness } = triangle
+      c.fillStyle = `rgba(${brightness * 128}, ${brightness * 128}, ${brightness * 128}, 1)`
+      c.beginPath()
+      c.moveTo(x + brightness * 32, y)
+      c.lineTo(x, y - brightness * 48)
+      c.lineTo(x - brightness * 32, y)
+      c.fill()
+      
+      triangle.y -= brightness / 2
+      if (triangle.y < -48 * brightness) {
+         triangle.y = canvas.height + 48 * brightness;
+      }
+   })
+   requestAnimationFrame(loop)
+}
+
+for (let i = 0; i < 32; i++) {
+   triangles.push({
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      brightness: 1 + Math.random()
+   })
+}
+
+window.addEventListener('mousemove', mouseHandler)
+
+loop()
+  </script>
+
+</body>
+</html>
+"""
 		self.write(html)
